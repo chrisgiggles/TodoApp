560ed8ac0bf589d61180779cab03f31eda64ceaf
Christo Georgiev <chrisgiggles@live.se>
2014-11-15 21:39:56 +0100

    added listview

--- js/app.js
+++ js/app.js
@@ -4,12 +4,17 @@ var Task = function(task, status, id) {
 	self.task   = ko.observable(task);
 	self.isDone = ko.observable(false);
 	self.id 	= id;
+	
+	self.taskStatus = ko.computed(function() {
+		if (self.isDone() === true) {
+			return "task-done"
+		}
+	});
 
 };
 
 var TaskViewModel = function() {
 	var self = this;
-
 	self.tasks = ko.observableArray([]);
 
 	self.textField = ko.observable();
@@ -19,7 +24,7 @@ var TaskViewModel = function() {
 	self.addTask = function() {
 		self.id += 1;
 		if (self.textField().trim() != "") {
-			self.tasks.push(new Task(self.textField(), false, self.id));
+			self.tasks.push(new Task(self.textField(), true, self.id));
 			self.textField("");
 		}
 		else {
@@ -40,39 +45,15 @@ var TaskViewModel = function() {
 		}
 	};
 	
-	this.taskStatus = ko.computed(function() {
-		for (var i = 0; i < self.tasks().length; i++) {
-			var isDone = self.tasks()[i].isDone();
-			console.log(isDone);
-			if (isDone === true) {
-				return "task-done";
-			}
-		}
-	});
-	
 	self.completion = ko.computed(function() {
 		var isDoneList = ko.utils.arrayFilter(self.tasks(), function(task) {
 			return task.isDone();
 		});
-		return "" + isDoneList.length + " of " + self.tasks().length + " tasks completed";
+		return isDoneList.length + 
+				" of " + 
+				self.tasks().length + 
+				" tasks completed";
 	});
 };
 
 ko.applyBindings(new TaskViewModel());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
